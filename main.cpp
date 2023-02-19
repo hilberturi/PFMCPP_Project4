62d7f891b8d4672df5552bb8ff0032349c390f76
hilberturi <122933097+hilberturi@users.noreply.github.com>
2023-02-19 08:33:15 +0000

    Proj. 4, Pt. 8: changed impl of cube() to produce exactly the expected log output

--- main.cpp
+++ main.cpp
@@ -532,8 +532,8 @@ void part7()
 template<typename Type>
 Type cube (std::unique_ptr<Type>& arg) 
 {
-    auto& v = *arg;
-    v *= v * v;
+    auto v = *arg;
+    *arg = v * v * v;
     return v;
 }
 
